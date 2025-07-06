c97d3224211e21ce8bb0e850e06fc4b20c341b0e
131MohitYadav <yadavjimohit54@gmail.com>
2025-07-06 20:53:08 +0530

    6 july 2025

---
+++ DSA_2025/src/dsa_2025/T5.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dsa_2025;
+
+/**
+ *
+ * @author apple
+ */
+public class T5 extends Thread {
+    
+    public void run(){
+        for ( int i = 1 ; i <= 5; i++){
+            System.out.println("Child Thread: " + i);
+        }
+    }
+    
+    public static void main(String args[]){
+        T5  t5 = new T5();
+        t5.start();
+        
+    }
+    
+}
